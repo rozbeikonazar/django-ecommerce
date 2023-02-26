1b97fd8aa1f4c0f3d5c4bf7e5253e649f93d30d6
Nazar <qwerty2004123@gmail.com>
2023-02-26 23:02:17 +0200

    Updated basic functionality of products app

--- products/views.py
+++ products/views.py
@@ -7,18 +7,24 @@ from django.template import loader
 
 
 def show_products(request, category_name=None):
+    """Show all products"""
     products = Product.objects.all()
     categories = Category.objects.all()
+
     if category_name:
-        products = Product.objects.filter(category__name=category_name) 
+        category = Category.objects.get(name=category_name)
+        title = category.name
+        products = products.filter(category=category)
+    else:
+        title = "Main"
+
     template = loader.get_template('products/show_products.html')
-    context = {'products': products, 'categories': categories}
+    context = {'products': products, 'categories': categories, 'title': title}
 
     return HttpResponse(template.render(context, request))
 
-
-
 def show_product(request, slug):
+    """Returns a product that user have chosen"""
     product = Product.objects.get(slug=slug)
     template = loader.get_template('products/product_details.html')
     context = {'product': product}
