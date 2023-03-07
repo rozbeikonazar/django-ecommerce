eddc162f56d77bf8781a9cfe6622b832a1b7521a
Ruslan Rozbeiko <ruslan.rozbeiko@aaaauto.cz>
2023-03-07 19:06:36 +0100

    added home

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -18,7 +18,8 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-urlpatterns = [
+from products import views
+urlpatterns = [   path('', views.show_products),
                   path('admin/', admin.site.urls),
                   path('products/', include('products.urls', namespace='products')),
                   path('accounts/', include('accounts.urls', namespace='accounts')),
