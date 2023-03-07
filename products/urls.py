5781130ccd2fc96f64a03d3a0cc28bc9ac2f4ee6
Nazar <qwerty2004123@gmail.com>
2023-03-07 16:45:06 +0200

    Fixed routes

--- products/urls.py
+++ products/urls.py
@@ -6,7 +6,7 @@ from .views import show_products, show_product, search_products
 app_name = 'products'
 #TODO FIX ROUTES for products
 urlpatterns = [
-    path('main/', show_products, name='product_list'),
+    path('', show_products, name='product_list'),
     path('details/<slug:slug>', show_product, name='product_details'),
     path('category/<str:category_name>/', show_products, name='category'),
     path('search_products', search_products, name='search_products'), 
