a9b7f8132146db00fdf3f0ee4776ea986c46fea4
Nazar <qwerty2004123@gmail.com>
2023-06-06 09:37:42 +0200

    Updated Product model

--- products/models.py
+++ products/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products_images/', blank=False)
+    image = models.ImageField(upload_to='products_images/', null=True)
     slug = models.SlugField(max_length=200,unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     
