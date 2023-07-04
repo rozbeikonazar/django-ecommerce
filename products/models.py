8d092cda82e90bb860858de844d6a1b29db039d6
Nazar <qwerty2004123@gmail.com>
2023-07-04 19:27:08 +0200

    feat(models): add quantity field to the model

--- products/models.py
+++ products/models.py
@@ -24,6 +24,7 @@ class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True)
     description = models.TextField(db_index=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, validators=[MinValueValidator(1)])
+    quantity = models.IntegerField(default=0)
     image = models.ImageField(upload_to='products_images/', null=True)
     slug = models.SlugField(max_length=200,unique=True, null=True, blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
